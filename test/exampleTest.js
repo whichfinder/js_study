b5c2408ddfaf9a2a74c27e2b5821c655686083c1
sasha <aleksandr.kurapin@gmail.com>
2017-08-03 12:52:31 +0300

    updated test

--- test/exampleTest.js
+++ test/exampleTest.js
@@ -1,10 +1,14 @@
 var test = require('unit.js');
-var js_start = require('../js_start.js')
+var js_start = require('../js_start.js');
+var rewire = require('rewire');
+var chai = require('chai');
+
+fact = js_start.__get__('factorial')
 
 describe ("function factorial", function() {
 
 	it("return expected result for value", function() {
-		var result = factorial(5);
+		var result = fact(5);
 
 		expect(result).toEqual(120);
 	});
