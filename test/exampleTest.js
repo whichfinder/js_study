46e5bec3ddf66394e0d77580eaf884dccf658627
sasha <aleksandr.kurapin@gmail.com>
2017-08-03 11:11:49 +0300

    fix

--- test/exampleTest.js
+++ test/exampleTest.js
@@ -1,8 +1,9 @@
 var test = require('unit.js');
+var js_start = require('../js_start.js')
 
 describe ("function factorial", function() {
 
-	it("return expected result for value" function{
+	it("return expected result for value", function() {
 		var result = factorial(5);
 
 		expect(result).toEqual(120);
