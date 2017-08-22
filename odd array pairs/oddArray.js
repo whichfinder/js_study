1321cac200b96079b7e97f63c7a8f054d6e84367
sasha <aleksandr.kurapin@gmail.com>
2017-08-22 18:01:14 +0300

    updated

--- odd array pairs/oddArray.js
+++ odd array pairs/oddArray.js
@@ -1,14 +1,17 @@
 function solution(A) {
 	A = JSON.parse(A)
-	while (A.length > 1) {
-		for (i = 1; i < A.length; i++) {
-	    	if (A[0] == A[i]) {
-	    		A.splice(0,1);
-	        	A.splice(i-1,1);
-	        	i = 1;
+	var result = [];
+	var a = {};
+	for (i = 1; i < A.length; i++) {
+			a['uniq'] = A[0];
+	    	if (A.includes(a['uniq'])) {
+	    		A.splice(A.indexOf(a['uniq']),1);
+	    		A.splice(A[a['uniq']],1);
+	    		a['uniq'] = A[0];
 	        }
-		}
-	}
-console.log(A);
-return A;
+	        else {
+	        	result.push(a['uniq'])
+	        }
+		return result;
+}
 }
